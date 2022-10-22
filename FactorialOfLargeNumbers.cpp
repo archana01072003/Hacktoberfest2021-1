9c332d6d2e23f190dbb74cdff3b6989b1e793b74
archana01072003 <85079777+archana01072003@users.noreply.github.com>
2022-10-23 00:43:27 +0530

    Create FactorialOfLargeNumbers.cpp

---
+++ FactorialOfLargeNumbers.cpp
@@ -0,0 +1,26 @@
+// C++ program to find large
+// factorials using BigInteger
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long
+
+// Returns Factorial of N
+ull factorial(int N)
+{
+
+	// Initialize result
+	ull f = 1; // Or BigInt 1
+
+	// Multiply f with 2, 3, ...N
+	for (ull i = 2; i <= N; i++)
+		f *= i;
+
+	return f;
+}
+int main()
+{
+	int N;
+    cin >> N;
+	cout << factorial(N) << endl;
+}
